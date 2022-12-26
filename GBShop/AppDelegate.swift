f0a1a054a00d1f2d81d626683cfdab187e4b1ea6
OksanaKam <moremore1@yandex.ru>
2022-12-26 16:35:41 +0200

    Создала классы для регистрации, авторизации, выхода, изменения личных данных.

--- GBShop/AppDelegate.swift
+++ GBShop/AppDelegate.swift
@@ -9,14 +9,31 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
+    var window: UIWindow?
+    let requestFactory = RequestFactory()
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions
+                     launchOptions:
+                     [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        let auth = requestFactory.makeAuthRequestFatory()
+        auth.login(userName: "Somebody", password: "mypassword") { response in
+            switch response.result {
+            case .success(let login):
+                print(login)
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+        return true
+    }
+/*
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
     }
-
+*/
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
---
+++ GBShop/RegisterUserResult.swift
@@ -0,0 +1,13 @@
+//
+//  RegisterUserResult.swift
+//  GBShop
+//
+//  Created by Оксана Каменчук on 26.12.2022.
+//
+
+import Foundation
+
+struct RegisterUserResult: Codable {
+    let result: Int
+    let userMessage: String
+}
